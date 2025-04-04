6803944eb2cc99bb1abee2f48f7402ee4b2f363f
Ofir Yahav <oyahav@ddn.com>
2025-04-04 06:02:22 +0300

    1

--- jenkinsfiles/remote_development.groovy
+++ jenkinsfiles/remote_development.groovy
@@ -1,15 +1,16 @@
-NODE_ARCH_MAP = [
-    'amd64_4vcpu_16gb_30gb' : 'amd64', 
-    'main_worker_arm64' : 'arm64', 
-    'deep_learning_worker_amd64' : 'amd64', 
-    'deep_learning_worker_arm64' : 'arm64'
-]
 
 node(env.node) {
     ansiColor('xterm') {
 
         def maxUptime = 80
         def uptimeInMinuts = env.uptime_in_minutes.toInteger()
+        def serviceSuffix = "_amd64"
+        if (env.node.toLowerCase().contains("arm64")) {
+            serviceSuffix = "_arm64"
+        }
+        if (env.node.toLowerCase().contains("gpu")) {
+            serviceSuffix = "${serviceSuffix}_gpu"
+        }
 
         if uptimeInMinuts > maxUptime {
             uptimeInMinuts = maxUptime
@@ -31,7 +32,7 @@ node(env.node) {
                 "GIT_REF=${env.ref}"
             ]) {   
                 sh "docker buildx create --name docker-container--driver docker-container --use --bootstrap"
-                sh "docker compose -f data_science/docker/docker-compose.yml build remote_dev_${NODE_ARCH_MAP[env.node]}"
+                sh "docker compose -f data_science/docker/docker-compose.yml build remote_dev_${serviceSuffix}"
             }
         }
 
@@ -42,7 +43,7 @@ node(env.node) {
                 withEnv([
                     "DOCKER_IMAGE_TAG=${env.BUILD_TAG}"
                 ]) {   
-                    sh "docker compose -f data_science/docker/docker-compose.yml run --service-ports remote_dev_${NODE_ARCH_MAP[env.node]}"
+                    sh "docker compose -f data_science/docker/docker-compose.yml run --service-ports remote_dev_${serviceSuffix}"
                 }
             }
         }
