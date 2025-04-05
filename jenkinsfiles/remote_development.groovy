adb56961a4f681cf69172525d57945195384211d
Ofir Yahav <oyahav@ddn.com>
2025-04-06 02:11:34 +0300

    1

--- jenkinsfiles/remote_development.groovy
+++ jenkinsfiles/remote_development.groovy
@@ -36,7 +36,7 @@ node(env.node) {
                 sh "aws ecr get-login-password --region eu-central-1 --profile OFIRYDEVOPS | docker login --username AWS --password-stdin 961341530050.dkr.ecr.eu-central-1.amazonaws.com"
                 sh "docker buildx create --name docker-container --driver docker-container --use --bootstrap"
                 sh "docker buildx ls"
-                sh "docker compose -f data_science/docker/docker-compose.yml build main_arm64_update_cache"
+                sh "docker compose -f data_science/docker/docker-compose.yml build main_arm64"
                 // sh "docker compose -f data_science/docker/docker-compose.yml build ${service}"
                 
             }
