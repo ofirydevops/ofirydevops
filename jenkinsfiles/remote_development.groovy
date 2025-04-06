464a14f0220392ae2cf01ba415c735299398224b
Ofir Yahav <oyahav@ddn.com>
2025-04-06 18:00:33 +0300

    1

--- jenkinsfiles/remote_development.groovy
+++ jenkinsfiles/remote_development.groovy
@@ -32,7 +32,7 @@ node(env.node) {
                 "DOCKER_IMAGE_TAG=${env.BUILD_TAG}",
                 "GIT_REF=${env.ref}"
             ]) {
-                sh "docker compose -f data_science/docker/docker-compose.yml build ${service} --builder dc"
+                // sh "docker compose -f data_science/docker/docker-compose.yml build ${service} --builder dc"
                 sh "docker compose -f data_science/docker/docker-compose.yml build main_amd64_update_cache --builder dc"
             }
         }
