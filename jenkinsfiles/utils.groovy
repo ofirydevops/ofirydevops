bee272f16acbe6f3d3da74fbfc0d2c7402273040
Ofir Yahav <oyahav@ddn.com>
2025-04-09 23:27:00 +0300

    1

---
+++ jenkinsfiles/utils.groovy
@@ -0,0 +1,15 @@
+def getDcServiceSuffix(dcServicePrefix, nodeLabel) {
+    def dcServiceSuffix = "_amd64"
+
+    def nodeLabelLowerCase = nodeLabel.toLowerCase()
+    if (nodeLabelLowerCase.contains("arm64")) {
+        dcServiceSuffix = "arm64"
+    }
+    if (nodeLabelLowerCase.contains("gpu")) {
+        dcServiceSuffix = "${serviceSuffix}_gpu"
+    }
+    def dcService = "${dcServicePrefix}${dcServiceSuffix}"
+    return dcService
+}
+
+return this
