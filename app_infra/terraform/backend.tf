612b949582656b093856634f173160a0164bd0cc
Ofir Yahav <yahavofir@gmail.com>
2025-11-21 16:02:58 +0200

    add app infra

---
+++ app_infra/terraform/backend.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "s3" {
+    key = "app-infra.tfstate"
+  }
+  required_version = ">= 1.5.7"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.20"
+    }
+  }
+}
